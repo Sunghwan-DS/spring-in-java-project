13e2d786fa614cc2e1870feac189203939696fbc
Sunghwan-DS <jeonsung02@gmail.com>
2024-01-26 12:09:30 +0900

    CREATE Spring in Java Project

---
+++ src/main/java/jsh/springinjavaproject/SpringInJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package jsh.springinjavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInJavaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInJavaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jsh/springinjavaproject/SpringInJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jsh.springinjavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInJavaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
